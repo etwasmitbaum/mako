3915eec3130636c0c767f9cd6737e455055ed3e1
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-12-27 18:34:53 -0800

    adding first test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,3 @@
-androidTestImplementation 'androidx.test.ext:junit:1.1.5'
-androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -14,8 +11,11 @@ android {
         applicationId = "com.rama.mako"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "20251226"
+        versionCode = 4
+        versionName = "20251228"
+
+        // Required for instrumentation tests
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -35,5 +35,14 @@ android {
 }
 
 dependencies {
+    // App
     implementation(libs.androidx.core.ktx)
+
+    // Local unit tests (JVM)
+    testImplementation("junit:junit:4.13.2")
+
+    // Instrumentation tests (device / emulator)
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
